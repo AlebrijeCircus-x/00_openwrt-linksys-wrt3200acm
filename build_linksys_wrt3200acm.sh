aabc21c8c66ceffb7bf5f9cd61b56e1bae881ab1
Ashley Barr <ashley.barr@ar4s.com>
2023-08-18 02:26:28 -0600

    Update build_linksys_wrt3200acm.sh

--- build_linksys_wrt3200acm.sh
+++ build_linksys_wrt3200acm.sh
@@ -21,7 +21,59 @@ cd openwrt-*/
 # clean previous images
 make clean
 
-make image  PROFILE="linksys_wrt3200acm" \
-           PACKAGES="block-mount kmod-fs-ext4 kmod-usb-storage blkid mount-utils swap-utils e2fsprogs fdisk luci dnsmasq" \
-           FILES="${BASEDIR}/files/" \
-           BIN_DIR="$OUTPUT"
+make image  	PROFILE="linksys_wrt3200acm"  		\
+		PACKAGES="block-mount 			\
+  			  kmod-fs-ext4 			\
+       			  kmod-usb-storage 		\
+	    		  blkid mount-utils 		\
+	 		  swap-utils 			\
+      			  e2fsprogs 			\
+	   		  fdisk 			\
+			  dnsmasq			\
+			  auc 				\
+			  base-files 			\
+			  busybox 			\
+			  ca-bundle 			\
+			  dnsmasq 			\
+			  dropbear 			\
+			  fail2ban			\
+			  firewall4 			\
+			  fstools 			\
+			  iwinfo 			\
+			  kmod-btmrvl 			\
+			  kmod-gpio-button-hotplug 	\
+			  kmod-mwifiex-sdio 		\
+			  kmod-mwlwifi 			\
+			  kmod-nft-offload 		\
+			  libustream-wolfssl 		\
+			  logd				\ 
+			  luci				\
+     			  luci-base 			\
+			  luci-app-firewall		\
+			  luci-app-advanced-reboot	\
+			  luci-app-attendedsysupgrade	\   	
+			  luci-app-nextdns 		\
+			  luci-app-opkg			\
+			  luci-app-squid 		\
+			  mtd 				\
+			  mwlwifi-firmware-88w8964 	\
+			  netifd 			\
+			  nftables 			\
+			  odhcp6c 			\	
+			  odhcpd-ipv6only 		\
+			  opkg 				\
+			  ppp 				\
+			  ppp-mod-pppoe 		\
+			  procd 			\
+			  procd-seccomp 		\
+			  procd-ujail 			\
+			  squid				\
+			  squid-mod-cachemgr		\
+			  uboot-envtools 		\
+			  uci 				\
+			  uclient-fetch 		\
+			  urandom-seed 			\
+			  urngd 			\
+			  wpad-basic-wolfssl 		\
+          	FILES="${BASEDIR}/files/" 		\
+          	BIN_DIR="$OUTPUT"
